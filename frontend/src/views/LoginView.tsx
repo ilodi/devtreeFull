ada10c6640abd2e2184f16f7e34059051617084a
lodi <lodi.dev@outlook.com>
2024-12-03 16:59:48 -0600

    front API DevTree

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,7 @@
+export default function LoginView(){
+  return(
+    <div className="text-6xl">
+      login
+    </div>
+  )
+}
---
+++ frontend/src/views/RegisterView.tsx
@@ -0,0 +1,7 @@
+export default function RegisterView(){
+  return(
+    <div className="text-6xl">
+      RegisterView
+    </div>
+  )
+}
